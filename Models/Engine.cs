efa6efb05877fbcf4e813e16e8840c38ce6f4ad5
Echuy <echuy@outlook.com>
2020-09-05 21:27:44 +0300

    Добавьте файлы проекта.

---
+++ Models/Engine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WnGCalculator.Models
+{
+    public class DnDObject
+    {
+        public int id { get; set; }
+        public Engine Type { get; set;}
+    }
+    public class Engine
+    {
+        public int id {get;}
+
+        public int ParentTag { get; set; }
+        public string Name { get; set; }
+
+        public string SysName { get; set; }
+
+        public string ImageName { get; set; }
+
+    }
+}
---
+++ Models/MonsterModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WnGCalculator.Models
+{
+    public class MonsterModel
+    {
+        /// <summary>
+        ///  Название чудовища
+        /// </summary>
+        string Name { get; set; }
+        /// <summary>
+        /// Уровень чудовища
+        /// </summary>
+        int Level { get; set}
+        
+        // TODO переделать в список ролей
+        string role;
+
+
+
+    }
+
+    
+}
